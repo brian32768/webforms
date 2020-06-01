e4316c6d59d2a02a3d9ff2608b369c0957fac341
Brian H Wilson <brian@wildsong.biz>
2020-06-01 12:20:57 -0700

    removed email logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,30 +2,10 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
 import logging
-from logging.handlers import SMTPHandler
+# I don't know where logging will go right now!
 
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
-
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Cases update failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-
-
-        print("sending mail via", app.config['MAIL_SERVER'])
-
 
 from app import routes
