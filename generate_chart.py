ce6ad5b9e20fb51fe5ee95145775e5c8ad088d13
bwilsoncc <bwilsoncc@co.clatsop.or.us>
2021-10-13 16:13:48 -0700

    re-read the data if we changed it

--- generate_chart.py
+++ generate_chart.py
@@ -16,17 +16,18 @@
 import os, sys
 import pandas as pd
 import numpy as np
-import arcgis.gis as GIS
-import arcgis.features as Features
+from arcgis.gis import GIS
+from arcgis.features import FeatureLayer, FeatureSet, Feature, Table
 from datetime import datetime
 import read_cases
 import plotly.graph_objects as go
+from config import Config
 
-
-def generate_chart(sdf):
+def generate_chart(cases_df):
     # https://github.com/d3/d3-3.x-api-reference/blob/master/Formatting.md#d3_format
-    cases = go.Bar(x=daily['date'], y=daily['cases'], name="Cases/day", marker_color="#f09665", hoverinfo="all")
-    avg   = go.Scatter(x=daily['date'], y=daily['avg'], name="7 day avg", marker_color="#671d85")
+    cases = go.Bar(x=cases_df['date'], y=cases_df['cases'], name="Cases/day", 
+        marker_color="#f09665", hoverinfo="all")
+    avg   = go.Scatter(x=cases_df['date'], y=cases_df['avg'], name="7 day avg", marker_color="#671d85")
     fig = go.Figure(data=[cases,avg])
     fig.update_xaxes(dtick="M1", tickformat="%b")
     fig.update_traces(hovertemplate="%{x|%b %d}: <b>%{y} cases</b>", selector=dict(type='bar'))
@@ -37,18 +38,68 @@ def generate_chart(sdf):
 
     return fig
 
+def update_database(current, last):
+    """ 
+Update the big database from the little one if the little one has new data for the big one.
+
+current = todays data
+last = the last row from the big database
+
+Returns True if the database was updated
+    """
+
+    # If these all match then the latest data
+    # has already been written to the database
+
+    if ((last.new_cases == current.new_cases) \
+        and (last.total_cases == current.total_cases) 
+        and (last.last_update == current.date)):
+        print("Database is current.")
+
+        # If you want to test the update, comment this out
+        # and then go in and manually delete the extra row(s).
+        return False
+
+    print("Appending the new record.")
+
+    attributes = {
+        "utc_date": datetime.utcnow(),
+        "last_update": current.date,
+        "editor": os.environ.get('USERNAME') or os.environ.get('USER'),
+        "source": "CC",
+        "new_cases" : current.new_cases,
+        "total_cases" : current.total_cases,
+        "new_deaths" : current.new_deaths,
+        "total_deaths" : current.total_deaths,
+        "name": "Clatsop"
+    }
+
+    gis = GIS(Config.PORTAL_URL, Config.PORTAL_USER, Config.PORTAL_PASSWORD)
+    layer = FeatureLayer(Config.COVID_CASES_URL, gis)
+    f = Feature.from_dict({"attributes": attributes})
+    fs = FeatureSet(features=[f])
+    results = layer.edit_features(adds=fs)
+
+    return True
+
 if __name__ == '__main__':
 
-    sdf = read_cases.read_daily_cases_df()
-    print(sdf)
+    local_sdf = read_cases.read_local_cases_df()
 
-    sdf = read_cases.read_local_cases_df()
-    (daily, total) = read_cases.clean_data(sdf, days = 30*4)
+    current = read_cases.read_current_cases_df().iloc[0]
+    print("Current data")
+    print(current)
+    last = local_sdf.sort_values("utc_date", ascending=False).iloc[0]
+    if update_database(current, last):
+        # Update happened so read the data again
+        local_sdf = read_cases.read_local_cases_df()
 
-    fig = generate_chart(sdf)
-    
+    days = 4*30
+    (daily_df, total_df) = read_cases.crunch_data(local_sdf, days)
+    fig = generate_chart(daily_df)
+   
     try:
         fig.write_html(sys.argv[1])
     except IndexError:
-        print("Give me a filename if you want HTML output.")
+        print("WARNING, you must give me a filename if you want HTML output.")
         fig.show()
